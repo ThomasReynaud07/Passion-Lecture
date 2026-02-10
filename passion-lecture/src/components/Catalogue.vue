39770ef6ac2a6bdd458c73370912dafad68e9d5d
Ryan Läuppi <pa33xxl@eduvaud.ch>
2026-02-10 16:50:23 +0100

    feat(Catalogue):Ajout de la page catalogue

---
+++ passion-lecture/src/components/Catalogue.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="container">
+    <h1>Bonjour</h1>
+  </div>
+</template>
+
+<style scoped>
+.container {
+  display: flex;
+
+  /* flex-start est le bon terme, pas "left" */
+
+  justify-content: flex-start;
+
+  align-items: flex-start;
+
+  /* On s'assure que la div prend toute la largeur */
+
+  width: 100%;
+
+  /* Optionnel : si un parent force le text-align: center */
+
+  text-align: left;
+
+  margin: 0;
+
+  padding: 20px; /* Un peu de padding pour pas que ça colle au bord */
+}
+</style>
