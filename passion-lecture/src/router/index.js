558593a0345ce208605e1105bf43b1d47a194ac2
Ryan Läuppi <pa33xxl@eduvaud.ch>
2026-02-24 17:24:15 +0100

    feat(ajouter un ouvrage):ajout de la page, route et du html css sans la logique

--- passion-lecture/src/router/index.js
+++ passion-lecture/src/router/index.js
@@ -4,6 +4,7 @@ import CatalogueView from '@/views/CatalogueView.vue'
 import DetailsView from '@/views/DetailsView.vue'
 import Loginview from '@/views/LoginView.vue'
 import SignupView from '@/views/SignupView.vue'
+import AjoutView from '@/views/AjoutView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -33,6 +34,11 @@ const router = createRouter({
       path: '/Signup',
       name: 'Signup',
       component: SignupView,
+    },
+    {
+      path: '/Ajout',
+      name: 'Ajout',
+      component: AjoutView,
     },
   ],
 })
