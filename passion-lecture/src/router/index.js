bfb9a8ed207c9c8fe24fd492b245dc94832d308c
Thomas <pk84vtk@eduvaud.ch>
2026-02-24 16:29:29 +0100

    feat(login/signup):Ajout de la page login et signup fictive

--- passion-lecture/src/router/index.js
+++ passion-lecture/src/router/index.js
@@ -2,6 +2,8 @@ import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 import CatalogueView from '@/views/CatalogueView.vue'
 import DetailsView from '@/views/DetailsView.vue'
+import Loginview from '@/views/LoginView.vue'
+import SignupView from '@/views/SignupView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -21,6 +23,16 @@ const router = createRouter({
       name: 'BookDetails',
       component: DetailsView,
       props: true,
+    },
+    {
+      path: '/Login',
+      name: 'login',
+      component: Loginview,
+    },
+    {
+      path: '/Signup',
+      name: 'Signup',
+      component: SignupView,
     },
   ],
 })
