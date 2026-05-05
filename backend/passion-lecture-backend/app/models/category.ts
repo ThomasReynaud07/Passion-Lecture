d668ee191bbe9682a28cf19f03e54983e3e21e14
Ryan Läuppi <pa33xxl@eduvaud.ch>
2026-05-05 15:28:06 +0200

    feat(seeders-models): Seeders and models

--- backend/passion-lecture-backend/app/models/category.ts
+++ backend/passion-lecture-backend/app/models/category.ts
@@ -1,25 +1,17 @@
 import { DateTime } from 'luxon'
-<<<<<<< HEAD
 import { BaseModel, column, hasMany } from '@adonisjs/lucid/orm'
 import Book from './book.js'
 import type { HasMany } from '@adonisjs/lucid/types/relations'
-=======
-import { BaseModel, column } from '@adonisjs/lucid/orm'
->>>>>>> 5bdf6d035b2b99bc98c5278b60237f548dab189c
 
 export default class Category extends BaseModel {
   @column({ isPrimary: true })
   declare id: number
 
   @column()
-<<<<<<< HEAD
   declare name: string
 
   @hasMany(() => Book)
   declare books: HasMany<typeof Book>
-=======
-  declare nom: string
->>>>>>> 5bdf6d035b2b99bc98c5278b60237f548dab189c
 
   @column.dateTime({ autoCreate: true })
   declare createdAt: DateTime
--- backend/passion-lecture-backend/app/models/comment.ts
+++ backend/passion-lecture-backend/app/models/comment.ts
@@ -1,21 +1,14 @@
 import { DateTime } from 'luxon'
-<<<<<<< HEAD
 import { BaseModel, belongsTo, column } from '@adonisjs/lucid/orm'
 import Book from './book.js'
 import type { BelongsTo } from '@adonisjs/lucid/types/relations'
 import User from './user.js'
-=======
-import { BaseModel, column, belongsTo } from '@adonisjs/lucid/orm'
-import User from './user.js'
-import { BelongsTo } from '@adonisjs/lucid/types/relations'
->>>>>>> 5bdf6d035b2b99bc98c5278b60237f548dab189c
 
 export default class Comment extends BaseModel {
   @column({ isPrimary: true })
   declare id: number
 
   @column()
-<<<<<<< HEAD
   declare content: string
 
   @column()
@@ -26,31 +19,16 @@ export default class Comment extends BaseModel {
 
   @belongsTo(() => Book)
   declare books: BelongsTo<typeof Book>
-=======
-  declare contenu: string
-
-  @column()
-  declare note: number
->>>>>>> 5bdf6d035b2b99bc98c5278b60237f548dab189c
 
   @column()
   declare userId: number
 
-<<<<<<< HEAD
   @belongsTo(() => User)
   declare user: BelongsTo<typeof User>
 
-=======
->>>>>>> 5bdf6d035b2b99bc98c5278b60237f548dab189c
   @column.dateTime({ autoCreate: true })
   declare createdAt: DateTime
 
   @column.dateTime({ autoCreate: true, autoUpdate: true })
   declare updatedAt: DateTime
-<<<<<<< HEAD
-=======
-
-  @belongsTo(() => User)
-  declare user: BelongsTo<typeof User>
->>>>>>> 5bdf6d035b2b99bc98c5278b60237f548dab189c
 }
