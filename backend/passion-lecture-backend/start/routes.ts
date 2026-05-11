465b64c71fd9f056ef4498958c0d280541711eb9
Ryan Läuppi <pa33xxl@eduvaud.ch>
2026-05-11 14:33:04 +0200

    feat(route):ajout des routes de comment

--- backend/passion-lecture-backend/start/routes.ts
+++ backend/passion-lecture-backend/start/routes.ts
@@ -10,6 +10,7 @@
 import BooksController from '#controllers/books_controller'
 import router from '@adonisjs/core/services/router'
 import AuthorsController from '#controllers/authors_controller'
+import CommentsController from '#controllers/comments_controller'
 router.get('/', async () => {
   return {
     hello: 'world',
@@ -19,3 +20,5 @@ router.get('/', async () => {
 router.resource('authors', AuthorsController).apiOnly()
 
 router.resource('books', BooksController).apiOnly()
+
+router.resource('comments', CommentsController).apiOnly()
