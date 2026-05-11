f1a426c0264ce7d0ae58972f0d21b5ed7bc72276
Ryan Läuppi <pa33xxl@eduvaud.ch>
2026-05-11 14:32:10 +0200

    feat(validator):ajout des champs userId et bookId au comment validator

--- backend/passion-lecture-backend/app/validators/comment.ts
+++ backend/passion-lecture-backend/app/validators/comment.ts
@@ -4,5 +4,7 @@ export const commentValidator = vine.compile(
   vine.object({
     content: vine.string().trim().maxLength(100),
     rate: vine.number().min(0).max(5),
+    userId: vine.number().exists({ table: 'users', column: 'id' }),
+    bookId: vine.number().exists({ table: 'books', column: 'id' }),
   })
 )
