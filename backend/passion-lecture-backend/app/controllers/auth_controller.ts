b0d088947c489620169dcc10ab6dab644f0087d6
Thomas <pk84vtk@eduvaud.ch>
2026-05-12 13:47:26 +0200

    feat: auth + books done
    Les tests bruno marchent avec login register et tout les books. Il est necessaire d'avoir un token pour toutes interaction avec un books, a changer plus tard pour get

---
+++ backend/passion-lecture-backend/app/controllers/auth_controller.ts
@@ -0,0 +1,31 @@
+import type { HttpContext } from '@adonisjs/core/http'
+import User from '#models/user'
+import { registerValidator, loginValidator } from '#validators/auth'
+
+export default class AuthController {
+  async register({ request, response }: HttpContext) {
+    const registerData = await request.validateUsing(registerValidator)
+
+    const user = await User.create(registerData)
+
+    const token = await User.accessTokens.create(user)
+
+    return response.created({
+      user: user.serialize(),
+      token: token.toJSON(),
+    })
+  }
+
+  async login({ request, response }: HttpContext) {
+    const { email, password } = await request.validateUsing(loginValidator)
+
+    const user = await User.verifyCredentials(email, password)
+
+    const token = await User.accessTokens.create(user)
+
+    return response.ok({
+      user: user.serialize(),
+      token: token.toJSON(),
+    })
+  }
+}
